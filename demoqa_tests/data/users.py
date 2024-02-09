e738e552a5dca65bf242134e8c947579cce80ceb
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-02-09 12:39:29 +0300

    upgrade test

--- demoqa_tests/data/users.py
+++ demoqa_tests/data/users.py
@@ -4,11 +4,13 @@ import dataclasses
 @dataclasses.dataclass
 class User:
     first_name: str
-    second_name: str
+    last_name: str
     email: str
     gender: str
     phone_number: str
-    date_of_birth: str
+    date_of_birth_year: str
+    date_of_birth_month: str
+    date_of_birth_day: str
     subjects: str
     hobbies: str
     picture: str
@@ -16,16 +18,18 @@ class User:
     state_city: str
 
 
-Oleg = User(
+oleg = User(
     first_name="Ivan",
-    second_name="Ivanov",
+    last_name="Ivanov",
     email="Ivanov@test.com",
     gender="Male",
     phone_number="1234567890",
-    date_of_birth="10 January,1980",
+    date_of_birth_year='1980',
+    date_of_birth_month='January',
+    date_of_birth_day='10',
     subjects="Physics",
     hobbies="Sports",
     picture="picture.jpg",
     current_address="111999, St Hall avenue 34",
-    state_city="Haryana Karnal"
+    state_city="Haryana Karnal",
 )
--- demoqa_tests/pages/registration_page.py
+++ demoqa_tests/pages/registration_page.py
@@ -3,6 +3,7 @@ from selene import browser, have, be
 from selene.core import command
 
 from demoqa_tests import resource
+from demoqa_tests.data.users import User
 
 
 class RegistrationPage:
@@ -10,54 +11,69 @@ class RegistrationPage:
     self.registered_user_data = browser.element(".table").all("td:nth-child(2)")
     """
 
-    @allure.step('Open maim page')
+    def __init__(self):
+        self.first_name = browser.element("#firstName") #.should(be.blank)
+        self.last_name = browser.element("#lastName") #.should(be.blank)
+        self.email = browser.element("#userEmail") #.should(be.blank)
+        self.gender = browser.all("[name=gender]")
+        self.mobile = browser.element("#userNumber") #.should(be.blank)
+        self.subjects = browser.element('#subjectsInput')
+        self.hobbies = browser.all("[type=checkbox]")
+        self.picture = browser.element("#uploadPicture")
+        self.current_address = browser.element("#currentAddress")
+        self.submit = browser.element("#submit")
+        self.subjects = browser.element('#subjectsInput')
+        self.picture = browser.element("#uploadPicture")
+
+
+    # @allure.step('Open maim page')
     def open(self):
         browser.open("/automation-practice-form")
-        # удаление рекламы - предусловие тестового сценария
         browser.all("[id^=google_ads][id$=container__]").with_(timeout=10).wait_until(
-            have.size_greater_than_or_equal(3)
-        )
+            have.size_greater_than_or_equal(3))
         browser.all("[id^=google_ads][id$=container__]").perform(command.js.remove)
 
-    @allure.step('Input first name {value}')
-    def fill_first_name(self, value):
-        browser.element("#firstName").should(be.blank).send_keys(value)
+    # @allure.step('Input first name {value}')
+    # def fill_first_name(self, value):
+    #     browser.element("#firstName").should(be.blank).send_keys(value)
+    #
+    # @allure.step('Input last name {value}')
+    # def fill_last_name(self, value):
+    #     browser.element("#lastName").should(be.blank).send_keys(value)
+    #
+    # @allure.step('Input e-mail {value}')
+    # def fill_email(self, value):
+    #     browser.element("#userEmail").should(be.blank).send_keys(value)
+    #
+    # @allure.step('Select gender {value}')
+    # def select_gender(self, value):
+    #     browser.all("[name=gender]").element_by(have.value(value)).element("..").click()
 
-    @allure.step('Input last name {value}')
-    def fill_last_name(self, value):
-        browser.element("#lastName").should(be.blank).send_keys(value)
+    # @allure.step('Input phone number {value}')
+    # def fill_phone_number(self, value):
+    #     browser.element("#userNumber").should(be.blank).send_keys(value)
 
-    @allure.step('Input e-mail {value}')
-    def fill_email(self, value):
-        browser.element("#userEmail").should(be.blank).send_keys(value)
-
-    @allure.step('Select gender {value}')
-    def select_gender(self, value):
-        browser.all("[name=gender]").element_by(have.value(value)).element("..").click()
-
-    @allure.step('Input phone number {value}')
-    def fill_phone_number(self, value):
-        browser.element("#userNumber").should(be.blank).send_keys(value)
-
-    @allure.step('Fill date of birth {year} {month} {day}')
-    def fill_date_of_birth(self, year, month, day):
+    # @allure.step('Fill date of birth {year} {month} {day}')
+    def fill_date_of_birth(self, user: User):
         browser.element("#dateOfBirthInput").click()
-        browser.element(".react-datepicker__year-select").send_keys(year)
-        browser.element(".react-datepicker__month-select").send_keys(month)
-        browser.element(f".react-datepicker__day--0{day}").click()
+        browser.element(".react-datepicker__year-select").send_keys(user.date_of_birth_year)
+        browser.element(".react-datepicker__month-select").send_keys(user.date_of_birth_month)
+        browser.element(f".react-datepicker__day--0{user.date_of_birth_day}").click()
 
-    @allure.step('Select hobbies {value}')
-    def select_hobbies(self, value):
+    # # @allure.step('Input subjects {value}')
+    # def type_subjects(self, value):
+    #     browser.element('#subjectsInput').type(value).press_enter()
+
+    # @allure.step('Select hobbies {value}')
+    def select_hobbies(self, user: User):
         browser.element("[for=hobbies-checkbox-2]").perform(command.js.scroll_into_view)
-        browser.all("[type=checkbox]").element_by(have.value(value)).element("..").click()
+        browser.all("[type=checkbox]").element_by(have.value(user.hobbies)).element("..").click()
 
     @allure.step('Upload picture with name {value}')
     def upload_picture(self, value):
         browser.element("#uploadPicture").send_keys(resource.path(value))
 
-    @allure.step('Input subjects {value}')
-    def type_subjects(self, value):
-        browser.element('#subjectsInput').type(value).press_enter()
+
 
     @allure.step('Input current address {value}')
     def type_current_address(self, value):
@@ -114,3 +130,22 @@ class RegistrationPage:
     @allure.step('Close modal window')
     def close_submiting_form(self):
         browser.element("#closeLargeModal").double_click()
+
+    def registration(self, user: User):
+        self.first_name.send_keys(user.first_name)
+        self.last_name.send_keys(user.last_name)
+        self.email.send_keys(user.email)
+        self.gender.element_by(have.value(user.gender)).element("..").click()
+        self.mobile.send_keys(user.phone_number)
+        self.fill_date_of_birth(user)
+        self.subjects.type(user.subjects).press_enter()
+        self.select_hobbies(user)
+        self.picture.send_keys(resource.path(user.picture))
+
+
+
+    # def registre(self, user: User):
+    #     self.first_name.send_keys(user.first_name)
+    #     self.last_name.send_keys(user.last_name)
+    #     self.email.send_keys(user.email)
+    #     self.gender.element_by(have.value(user.gender)).element("..").click()
