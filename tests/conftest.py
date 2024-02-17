da9408f5836292f0142e28acd070b9d6517410d6
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-02-17 12:56:27 +0300

    update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,11 +24,11 @@ def driver_configuration():
             options=options)
 
         browser.config.driver = driver
-        driver_options = webdriver.ChromeOptions()
-        driver_options.page_load_strategy = 'eager'
-        browser.config.driver_options = driver_options
-        browser.config.window_width = 1920
-        browser.config.window_height = 1080
+        # driver_options = webdriver.ChromeOptions()
+        # driver_options.page_load_strategy = 'eager'
+        # browser.config.driver_options = driver_options
+        # browser.config.window_width = 1920
+        # browser.config.window_height = 1080
         browser.config.base_url = "https://demoqa.com"
 
     yield
