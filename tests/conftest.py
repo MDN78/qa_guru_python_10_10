a42a533428a9f729a9d2ca0a389d0bd23e7a5774
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-02-17 12:25:56 +0300

    add eager mode to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,15 @@
 import allure
 import pytest
 from selene import browser
+from selenium import webdriver
 
 
 @pytest.fixture(scope="function", autouse=True)
 def driver_configuration():
     with allure.step('Driver configuration'):
+        driver_options = webdriver.ChromeOptions()
+        driver_options.page_load_strategy = 'eager'
+        browser.config.driver_options = driver_options
         browser.config.window_width = 1920
         browser.config.window_height = 1080
         browser.config.base_url = "https://demoqa.com"
